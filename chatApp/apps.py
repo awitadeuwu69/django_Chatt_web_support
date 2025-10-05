32546b5e22e6d22dcf770d0b0427465de9a16b7b
awitadeuwu69 <axel_slash@hotmail.cl>
2025-10-05 20:38:02 -0300

    comentario

--- chatApp/apps.py
+++ chatApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig #ola
 
 
-class ChatappConfig(AppConfig):
+class ChatappConfig(AppConfig): #app
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chatApp'
